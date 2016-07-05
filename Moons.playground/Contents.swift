c35a56e98f295203c157bf83b05be70aa3a8fb35
eldooon <eldonchan91@gmail.com>
2016-07-05 15:13:07 -0400

    done

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -20,11 +20,13 @@ planetDescription(planet)
 */
 
 
+//function needs to incoming method
 func greet(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
 
+//new working function that takes in two arguement with specified value
 func planetDescription(planet: String, numberOfMoons: Int) {
     print("There are \(numberOfMoons) moons orbiting \(planet)")
 }
@@ -33,11 +35,14 @@ func planetDescription(planet: String, numberOfMoons: Int) {
 // Uncomment it to see what the error is!
 //planetDescription("Jupiter", 67)
 
+//second argument needs to be specified
 planetDescription("Jupiter", numberOfMoons: 67)
 
 var planet = "Jupiter"
 var moons = 67
 planetDescription(planet, numberOfMoons: moons)
+
+//redefine variables
 planet = "Mars"
 moons = 2
 planetDescription(planet, numberOfMoons: moons)
